bbfd0d927939346ca09678dc57f9b89c95b8604a
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2024-12-12 22:04:49 +0200

    update

--- js/01-args.js
+++ js/01-args.js
@@ -2,42 +2,82 @@
  * Псевдомасив arguments и Array.from
  */
 
-function foo() {
-  console.log(arguments);
-  const args = Array.from(arguments);
-  console.log(args);
-}
+//!======================================================
 
-fn(1, 2, 3);
-fn(1, 2, 3, 4, 5);
-fn(1, 2, 3, 4, 5, 6, 7);
+// function foo() {
+//   const arr = Array.from(arguments); // [10, 20, 30]
+//   const includesZero = arr.includes(0); // false
+//   return includesZero;
+// }
+
+// const res1 = foo(10, 20, 30);
+// const res2 = foo(10, 20, 30, 0);
+
+// console.log(res1);
+// console.log(res2);
+
+//!======================================================
+
+// Math.max(10,10,340,601,23,2)
+// Math.min();
+// [].push('Hello', 'World', 'test')
+
+//!======================================================
+// function fn(x,y,q) {
+//   const args = Array.from(arguments);
+//   console.log(args);
+// }
+
+// fn(1, 2, 3);
+// fn(1, 2, 3, 4, 5);
+// fn(1, 2, 3, 4, 5, 6, 7);
+
+//!======================================================
 
 /**
  * Напиши функцію add для складання довільної
  * кількості аргументів (чисел)
  */
 
-const add = function (arguments) {
-  const args = Array.from(arguments);
-  let total = 0;
+// function sum() {
+//   let total = 0;
+//   for (const item of arguments) {
+//     total += item;
+//   }
+//   return total;
+// }
 
-  for (const arg of args) {
-    total += arg;
-  }
+// const res1 = sum(10, 20, 30, 40);
+// const res2 = sum(50, 60);
+// const res3 = sum(50);
+// const res4 = sum();
 
-  return total;
-};
+// console.log(res1);
 
-console.log(add(1, 2, 3));
-console.log(add(1, 2, 4, 5, 6));
-
+//!======================================================
 /**
  * Напиши функцію calAverage() яка приймає довільну кількість
  * аргументів і повертає їхнє середнє значення. Усі аргументи
  * будуть лише числами.
  */
-function calAverage() {}
+
+function calAverage() {
+  const arr = Array.from(arguments);
+  let total = 0;
+
+  for (const item of arr) {
+    total += item;
+  }
+
+  return total / arr.length;
+}
 
 console.log(calAverage(1, 2, 3, 4)); // 2.5
 console.log(calAverage(14, 8, 2)); // 8
 console.log(calAverage(27, 43, 2, 8, 36)); // 23.2
+
+//!======================================================
+
+//!======================================================
+
+const x = 500;
--- js/03-stack.js
+++ js/03-stack.js
@@ -2,27 +2,60 @@
  * - Стек викликів
  * - Порядок виконання
  */
+//!======================================================
 
-function fnA() {
-  console.log("fnA execution");
+function foo1() {
+  console.log('start1');
+  foo2();
+  console.log('end1');
+}
+function foo2() {
+  console.log('start2');
+  foo3();
+  console.log('end2');
+}
+function foo3() {
+  console.log('start3');
+  foo4();
+  console.log('end3');
+}
+function foo4() {
+  console.log('start4');
+  console.log(test);
+  console.log('end4');
 }
 
-function fnB() {
-  console.log("fnB execution");
-}
+//!======================================================
 
-function fnC() {
-  console.log("fnC execution");
-}
+// function foo(i) {
+//   console.log(i);
+//   foo(i + 1);
+// }
 
-console.log("Before fnA execution");
-fnA();
-console.log("After fnA execution");
+// foo(0);
 
-console.log("Before fnB execution");
-fnB();
-console.log("After fnB execution");
+//!======================================================
 
-console.log("Before fnC execution");
-fnC();
-console.log("After fnC execution");
+// function fnA() {
+//   console.log("fnA execution");
+// }
+
+// function fnB() {
+//   console.log("fnB execution");
+// }
+
+// function fnC() {
+//   console.log("fnC execution");
+// }
+
+// console.log("Before fnA execution");
+// fnA();
+// console.log("After fnA execution");
+
+// console.log("Before fnB execution");
+// fnB();
+// console.log("After fnB execution");
+
+// console.log("Before fnC execution");
+// fnC();
+// console.log("After fnC execution");
