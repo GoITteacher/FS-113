36351a971707c52f3f88a423aab7ee0e830b6d35
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-01-28 22:14:51 +0200

    update

--- js/02-feedback.js
+++ js/02-feedback.js
@@ -1,20 +1,79 @@
-const STORAGE_KEY = "feedback-msg";
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.css';
 
-const form = document.querySelector(".feedback-form");
-const textarea = form.querySelector("textarea");
+// new SimpleLightbox('.some-element a', { /* options */ });
 
-/*
- * - Скасовуємо стандартну поведінку
- * - Видаляємо повідомлення зі сховища
- * - Очищуємо форму
- */
+const STORAGE_KEY = 'feedback-msg';
 
-/*
- * - Отримуємо значення поля
- * - Зберігаємо його у сховище
- */
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  container: document.querySelector('.js-container'),
+};
 
-/*
- * - Отримуємо значення зі сховища
- * - Якщо там щось було, оновлюємо DOM
- */
+let arr = [];
+
+//!======================================================
+
+refs.form.addEventListener('input', e => {
+  const name = e.currentTarget.elements.name.value;
+  const message = e.currentTarget.elements.message.value;
+  const data = { name, message };
+  saveToLS(STORAGE_KEY, data);
+});
+
+//!======================================================
+
+function initPage() {
+  const formData = loadFromLS(STORAGE_KEY);
+  refs.form.elements.name.value = formData?.name || 'Anonym';
+  refs.form.elements.message.value = formData?.message || '';
+
+  arr = loadFromLS('data') || [];
+  refs.container.innerHTML = cardsTemplate(arr);
+}
+
+initPage();
+
+//!======================================================
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  const name = e.currentTarget.elements.name.value;
+  const message = e.currentTarget.elements.message.value;
+  const data = { name, message };
+
+  arr.push(data);
+
+  refs.container.innerHTML = cardsTemplate(arr);
+  saveToLS('data', arr);
+  localStorage.removeItem(STORAGE_KEY);
+  e.target.reset();
+});
+
+//!======================================================
+
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key) {
+  const body = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(body);
+    return data;
+  } catch {
+    return body;
+  }
+}
+
+function cardTemplate(card) {
+  return `<div>
+        <p>${card.name}</p>
+        <p>${card.message}</p>
+      </div>`;
+}
+
+function cardsTemplate(cards) {
+  return cards.map(cardTemplate).join('\n');
+}
---
+++ js/json.js
@@ -0,0 +1,75 @@
+//!======================================================
+// 12
+// "awdawd"
+// true
+// false
+// null
+// []
+// {}
+
+//!======================================================
+// JSON
+// const str = `{
+//  "update_id": 936980935,
+//  "message": {
+//   "message_id": 1895449,
+//   "from": {
+//    "id": 433982686,
+//    "is_bot": false,
+//    "first_name": "Volodymyr",
+//    "username": "pashchenko_v_r",
+//    "language_code": "uk",
+//    "is_premium": true
+//   },
+//   "chat": {
+//    "id": 433982686,
+//    "first_name": "Volodymyr",
+//    "username": "pashchenko_v_r",
+//    "type": "private"
+//   },
+//   "date": 1738087566,
+//   "text": "test"
+//  }
+// }`;
+
+// const message = JSON.parse(str);
+// console.log(message);
+
+//=================
+// const x = [10, 20, 30, 40];
+
+// const data = JSON.stringify(x);
+
+// console.log(data);
+// console.log(x);
+
+// const data = { name: 'Hello', id: undefined, sayHello() {} };
+// // const data = [10, 20, undefined, 40];
+
+// const str = JSON.stringify(data);
+
+// console.log(str);
+
+//!======================================================
+// console.log('START');
+
+// try {
+//   const userData = prompt('Enter your obj:'); // 'Hello woerld'
+//   const result = JSON.parse(userData);
+//   console.log(result);
+// } catch (err) {
+//   console.dir(err);
+// }
+
+// console.log('END');
+
+//!======================================================
+// try {
+
+// } catch {
+//   console.log('щось пішло не так...');
+// }
+
+// console.log('The end');
+
+//!======================================================
